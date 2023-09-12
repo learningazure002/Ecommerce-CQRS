fe677b041a32e6ca3a04148452a9e188d0dfa8f3
Mission Azure Developer <144518368+learningazure002@users.noreply.github.com>
2023-09-12 12:25:17 +0530

    Update Program.cs

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddTransient<ICustomerCommandRepository, CustomerCommandReposit
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c => {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1")
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1");
     c.RoutePrefix = string.Empty;
 });
 
